6d7ed50ddda51416fa40de8915608a472e75795c
Ace <ace.jckim@gmail.com>
2015-10-20 18:47:47 +0900

    DeviceControlInfo의 모델을 변경함.

---
+++ RowIRSender/app/src/main/java/com/gdgssu/rowirsender/DeviceControlInfo.java
@@ -0,0 +1,9 @@
+package com.gdgssu.rowirsender;
+
+import com.lge.hardware.IRBlaster.Device;
+
+public class DeviceControlInfo {
+    public int deviceId;
+    public String functionName;
+    public String deviceName;
+}
