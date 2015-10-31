b4a8678ad9210490d211525678ec1e2191173c72
강경완 <lk@lemonlab.co.kr>
2015-10-31 20:22:14 +0900

    Json 데이터 추가

---
+++ RowClient/mobile/src/main/java/com/gdgssu/rowclient/model/Person.java
@@ -0,0 +1,12 @@
+package com.gdgssu.rowclient.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lk on 15. 10. 31..
+ */
+public class Person {
+    public String person_name;
+    public ArrayList<Program> person_sche = new ArrayList<>();
+
+}
